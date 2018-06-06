64829116223c525b0264489030d875724965f68b
Moolick <35791332+Moolick@users.noreply.github.com>
2018-06-05 23:14:41 -0700

    Add files via upload

--- Matlab/partition.m
+++ Matlab/partition.m
@@ -1,6 +1,17 @@
-function [out] = partition(in)
+function [out] = partition(in, left, right, pivot)
 
-if size(in) < 2
+if left >= right
+    out = left;
     return
 end
 
+
+
+if in(left) > pivot
+    right = right - 1;
+    in = swap(in, left, right);
+    out = partition(in, left, right, pivot);
+else
+    left = left + 1;
+    out = partition(in, left, right, pivot);
+end
